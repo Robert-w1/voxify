7a1232b6d4e00cadedf310f4c821c0e2c03494a0
Robert Winkler <73788144+Robert-w1@users.noreply.github.com>
2026-03-11 13:17:07 +0800

    add recording saved in cache

--- app/javascript/controllers/recorder_controller.js
+++ app/javascript/controllers/recorder_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="recorder"
 export default class extends Controller {
-  static targets = ["status"]
+  static targets = ["microphoneStatus", "recordingStatus"]
 
   connect() {
     this.requestMicrophone()
@@ -11,10 +11,41 @@ export default class extends Controller {
   async requestMicrophone() {
     try {
       this.stream = await navigator.mediaDevices.getUserMedia({ audio: true })
-      this.statusTarget.textContent = "Microphone access granted 🎤"
+      this.microphoneStatusTarget.textContent = "Microphone access granted 🎤"
     } catch (error) {
       console.error("Microphone permission denied:", error)
-      this.statusTarget.textContent = "Microphone access denied ❌"
+      this.microphoneStatusTarget.textContent = "Microphone access denied ❌"
     }
+  }
+
+  start() {
+    this.chunks = []
+
+    this.mediaRecorder = new MediaRecorder(this.stream)
+
+    this.mediaRecorder.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        this.chunks.push(event.data)
+      }
+    }
+
+    this.mediaRecorder.onstop = () => {
+      this.audioBlob = new Blob(this.chunks, { type: "audio/webm" })
+      console.log("Recorded blob:", this.audioBlob)
+
+      // for testing: playback
+      const audioURL = URL.createObjectURL(this.audioBlob)
+      const audio = new Audio(audioURL)
+      audio.controls = true
+      document.body.appendChild(audio)
+    }
+
+    this.mediaRecorder.start()
+    this.recordingStatusTarget.textContent = "Recording..."
+  }
+
+  stop() {
+    this.mediaRecorder.stop()
+    this.recordingStatusTarget.textContent = "Recording stopped"
   }
 }
