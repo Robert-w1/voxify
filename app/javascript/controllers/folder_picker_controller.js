c113751108f6957eb5a48873e9ee45299931d726
Yasmine Fai <yfaingna@gmail.com>
2026-03-16 21:14:18 +0800

    Finalised everything for folders

---
+++ app/javascript/controllers/folder_picker_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["popup"]
+
+  connect() {
+    this._boundClose = this._closeOnOutsideClick.bind(this)
+    document.addEventListener("mousedown", this._boundClose)
+  }
+
+  disconnect() {
+    document.removeEventListener("mousedown", this._boundClose)
+  }
+
+  toggle(e) {
+    e.preventDefault()
+    e.stopPropagation()
+    this.popupTarget.hidden = !this.popupTarget.hidden
+  }
+
+  close() {
+    this.popupTarget.hidden = true
+  }
+
+  _closeOnOutsideClick(e) {
+    if (!this.element.contains(e.target)) this.close()
+  }
+}
--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,4 +1,10 @@
 class Folder < ApplicationRecord
+  include PgSearch::Model
+
+  pg_search_scope :search_by_name,
+    against: { name: "A", description: "B" },
+    using: { tsearch: { prefix: true } }
+
   belongs_to :user
 
   has_many :recording_sessions, dependent: :nullify
